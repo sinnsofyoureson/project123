9cccff75cb077ff36e3868d3a43b061e5afa7ec5
Rinat <sinnsofvalkyrie@gmail.com>
2019-02-02 04:47:14 +0400

    Updates

---
+++ precache-manifest.8bf45ae0b42dc81afaea92810481df29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02ad7472acda6ff9d86e",
+    "url": "/project123/static/css/main.eb922290.chunk.css"
+  },
+  {
+    "revision": "02ad7472acda6ff9d86e",
+    "url": "/project123/static/js/main.02ad7472.chunk.js"
+  },
+  {
+    "revision": "266251681fc1a9f23dd0",
+    "url": "/project123/static/js/1.26625168.chunk.js"
+  },
+  {
+    "revision": "98c1bb1f850edd582f36",
+    "url": "/project123/static/js/runtime~main.98c1bb1f.js"
+  },
+  {
+    "revision": "f3b674b4b05a199467f15cd957c1c6d4",
+    "url": "/project123/index.html"
+  }
+];
